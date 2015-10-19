3e80b32343bdefdfeb32c3374e8a95fafc5d61db
root <root@oldschoolvalue.com>
2015-10-19 09:49:16 -0400

    Change criteria for selecting ADRs

--- crons/include/raw_data_update_queries.php
+++ crons/include/raw_data_update_queries.php
@@ -639,7 +639,7 @@ function update_raw_data_tickers($dates, $rawdata) {
 	$stock_type = "ADR";
 	$MRQRow = 10;
 	$PMRQRow = 9;
-	if($rawdata["FormType"][10] == "10-K" || $rawdata["FormType"][10] == "10-Q" || $rawdata["FormType"][10] == "8-k" || $rawdata["FormType"][10] == "424B4") {
+	if($rawdata["Country"] == "UNITED STATES OF AMERICA") {
 		$stock_type = "USA";
 		$MRQRow = 26;
 		$PMRQRow = 25;
