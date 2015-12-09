e9a748e4f236582954555a6a7179a9ce50f264ea
PerhunAndrey <perhun@softproject.com.ua>
2015-12-09 11:33:44 +0200

    Altering db engine to InnoDB for development databases

---
+++ database/jjun0366_developers2/MD.developers2.20151205.01.alter_engine.PAP.sql
@@ -0,0 +1,7 @@
+ALTER TABLE eol_control ENGINE=InnoDB;
+ALTER TABLE eol_info ENGINE=InnoDB;
+ALTER TABLE eol_reports ENGINE=InnoDB;
+ALTER TABLE gf_info ENGINE=InnoDB;
+ALTER TABLE gf_reports ENGINE=InnoDB;
+ALTER TABLE osv_dlsessions ENGINE=InnoDB;
+ALTER TABLE system ENGINE=InnoDB;
