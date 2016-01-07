db88ce6d924d0f9c3fa19e664d563cacc4f5e6d0
root <root@oldschoolvalue.com>
2016-01-07 15:11:15 -0500

    Add necesary data for Growth Rating calculation

---
+++ database/jjun0366_frontend/MD.frontend.20160104.01.alter_financialscustom.SEK.sql
@@ -0,0 +1,3 @@
+ALTER TABLE  `reports_financialscustom` ADD  `SalesPercChange` DECIMAL( 30, 15 ) NULL ,
+ADD  `Sales5YYCGrPerc` DECIMAL( 30, 15 ) NULL;
+ALTER TABLE  `ttm_financialscustom` ADD  `Sales5YYCGrPerc` DECIMAL( 30, 15 ) NULL;
