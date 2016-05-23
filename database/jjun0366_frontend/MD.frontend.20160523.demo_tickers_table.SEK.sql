5a408e9cd32a91fbde98a65cea8311fa8ffd654f
Sergio <skarsvnie@gmail.com>
2016-05-23 09:23:25 -0400

    OSA-330 make a 'demo' version of the app with demo data

---
+++ database/jjun0366_frontend/MD.frontend.20160523.demo_tickers_table.SEK.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS `demo_tickers` (`ticker` varchar(20) NOT NULL, PRIMARY KEY (`ticker`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `demo_tickers` (`ticker`) VALUES ('AAPL'),('BAC'),('MSFT');
