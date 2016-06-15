622a6053100486774bfbebee848b75bdbddfdaed
Sergio <skarsvnie@gmail.com>
2016-06-15 10:29:25 -0400

    OSA-488 TTM for last ticket is not being calculated

--- crons/include/update_quality_checks.php
+++ crons/include/update_quality_checks.php
@@ -124,73 +124,10 @@ function update_pio_checks() {
 
 		//Update TTM Data
 		if($idChange && !$first) {
-			$queryqtr = "SELECT * FROM reports_header where report_type='QTR' and ticker_id = $ppid order by fiscal_year desc, fiscal_quarter desc limit 1";
-			$resqtr = mysql_query($queryqtr) or die (mysql_error());
-			$rowqtr =  mysql_fetch_assoc($resqtr);
-			if ($rowqtr["fiscal_year"] == $prawdata["fiscal_year"] && $rowqtr["fiscal_quarter"] == $prawdata["fiscal_quarter"]) {
-				$query1 = "INSERT INTO `ttm_pio_checks` (`ticker_id`, `pio1`, `pio2`, `pio3`, `pio4`, `pio5`, `pio6`, `pio7`, `pio8`, `pio9`, `pioTotal`) VALUES (";
-                                $query1 .= "'".$ppid."',";
-               			mysql_query($query1.$querypre) or die (mysql_error());
-			} else {
-				$total = 0;
-
-				$tquery = "SELECT * FROM `ttm_balanceconsolidated` a, ttm_balancefull b, ttm_cashflowconsolidated c, ttm_cashflowfull d, ttm_financialscustom e, ttm_incomeconsolidated f, ttm_incomefull g, ttm_gf_data h WHERE a.ticker_id=b.ticker_id AND a.ticker_id=c.ticker_id AND a.ticker_id=d.ticker_id AND a.ticker_id=e.ticker_id AND a.ticker_id=f.ticker_id AND a.ticker_id=g.ticker_id and a.ticker_id=h.ticker_id and a.ticker_id = $ppid";
-        			$tres = mysql_query($tquery) or die (mysql_error());
-				$trawdata = mysql_fetch_assoc($tres);
-		                $query1 = "INSERT INTO `ttm_pio_checks` (`ticker_id`, `pio1`, `pio2`, `pio3`, `pio4`, `pio5`, `pio6`, `pio7`, `pio8`, `pio9`, `pioTotal`) VALUES (";
-        		        $query1 .= "'".$ppid."',";
-                		//Pio 1
-		                $value = (!is_null($trawdata["IncomebeforeExtraordinaryItems"]) && $trawdata["IncomebeforeExtraordinaryItems"] >= 0 ? 1 : 0);
-        		        $total += $value;
-                		$query = "'".($value)."',";
-	                	//Pio 2
-	        	        $value = (!is_null($trawdata["CashfromOperatingActivities"]) && $trawdata["CashfromOperatingActivities"] >= 0 ? 1 : 0);
-        	        	$total += $value;
-	        	        $query .= "'".($value)."',";
-        	        	//Pio 3
-	                        $vn = (is_null($trawdata["TotalAssets"]) || $trawdata["TotalAssets"] == 0) ? 0 : ($trawdata["IncomebeforeExtraordinaryItems"]/$trawdata["TotalAssets"]);
-        	                $vv = (is_null($prawdata["TotalAssets"]) || $prawdata["TotalAssets"] == 0) ? 0 : ($prawdata["IncomebeforeExtraordinaryItems"]/$prawdata["TotalAssets"]);
-                	        $value = ($vn >= $vv ? 1 : 0);
-                        	$total += $value;
-	                        $query .= "'".($value)."',";
-		                //Pio 4
-        		        $value = ($trawdata["CashfromOperatingActivities"] >= $trawdata["IncomebeforeExtraordinaryItems"] ? 1 : 0);
-                		$total += $value;
-		                $query .= "'".($value)."',";
-        	                //Pio 5
-                	        $vn = (($trawdata["TotalAssets"]+$prawdata["TotalAssets"]) == 0) ? 0 : (($trawdata["TotalLongtermDebt"])/(($trawdata["TotalAssets"]+$prawdata["TotalAssets"])/2));
-                        	$vv = (($prawdata["TotalAssets"]+$pprawdata["TotalAssets"]) == 0) ? 0 : (($prawdata["TotalLongtermDebt"])/(($prawdata["TotalAssets"]+$pprawdata["TotalAssets"])/2));
-	                        $value = ($vn <= $vv ? 1 : 0);
-        	                $total += $value;
-                	        $query .= "'".($value)."',";
-                        	//Pio 6
-	                        $vn = (is_null($trawdata["TotalCurrentLiabilities"]) || $trawdata["TotalCurrentLiabilities"] == 0) ? 0 : ($trawdata["TotalCurrentAssets"]/$trawdata["TotalCurrentLiabilities"]);
-        	                $vv = (is_null($prawdata["TotalCurrentLiabilities"]) || $prawdata["TotalCurrentLiabilities"] == 0) ? 0 : ($prawdata["TotalCurrentAssets"]/$prawdata["TotalCurrentLiabilities"]);
-                	        $value = ($vn >= $vv ? 1 : 0);
-                        	$total += $value;
-	                        $query .= "'".($value)."',";
-        	                //Pio 7
-                	        $value = (toFloat($trawdata["SharesOutstandingDiluted"]) <= toFloat($prawdata["SharesOutstandingDiluted"]) ? 1 : 0);
-                        	$total += $value;
-	                        $query .= "'".($value)."',";
-        	                //Pio 8
-                	        $vn = (is_null($trawdata["TotalRevenue"]) || $trawdata["TotalRevenue"] == 0) ? 0 : ($trawdata["GrossProfit"]/$trawdata["TotalRevenue"]);
-                        	$vv = (is_null($prawdata["TotalRevenue"]) || $prawdata["TotalRevenue"] == 0) ? 0 : ($prawdata["GrossProfit"]/$prawdata["TotalRevenue"]);
-	                        $value = ($vn >= $vv ? 1 : 0);
-        	                $total += $value;
-                	        $query .= "'".($value)."',";
-                        	//Pio 9
-	                        $vn = (is_null($prawdata["TotalAssets"]) || $prawdata["TotalAssets"] == 0) ? 0 : ($trawdata["TotalRevenue"]/$prawdata["TotalAssets"]);
-        	                $vv = (is_null($pprawdata["TotalAssets"]) || $pprawdata["TotalAssets"] == 0) ? 0 : ($prawdata["TotalRevenue"]/$pprawdata["TotalAssets"]);
-                	        $value = ($vn >= $vv ? 1 : 0);
-                        	$total += $value;
-	                        $query .= "'".($value)."',";
-		                $query .= "'".($total)."'";
-        		        $query .= ")";
-               			mysql_query($query1.$query) or die (mysql_error());
-			}
+			pioTTM($ppid,$prawdata,$querypre,$pprawdata);
 		}
 	}
+	pioTTM($pid,$rawdata,$query2,$prawdata);
 }
 
 function update_altman_checks() {
@@ -252,50 +189,122 @@ function update_altman_checks() {
 
                 //Update TTM Data
                 if($idChange && !$first) {
-                        $tquery = "SELECT * FROM ttm_incomeconsolidated a, ttm_balanceconsolidated b, ttm_gf_data c WHERE a.ticker_id=b.ticker_id AND a.ticker_id=c.ticker_id AND a.ticker_id= " . $ppid;
-                        $tres = mysql_query($tquery) or die (mysql_error());
-                        $trawdata = mysql_fetch_assoc($tres);
-                        array_walk_recursive($trawdata, 'nullValues');
-                        $query1 = "INSERT INTO `ttm_alt_checks` (`ticker_id`, `WorkingCapital`, `TotalAssets`, `TotalLiabilities`, `RetainedEarnings`, `EBIT`, `SharesOutstandingDiluted`, `NetSales`, `X1`, `X2`, `X3`, `X5`) VALUES (";
-                        $query1 .= "'".$ppid."',";
-
-                        $query1 .= ($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"]) . ",";
-                        $query1 .= $trawdata["TotalAssets"] . ",";
-                        $query1 .= $trawdata["TotalLiabilities"] . ",";
-                        $query1 .= $trawdata["RetainedEarnings"] . ",";
-                        $query1 .= $trawdata["EBIT"] . ",";
-                        $query1 .= toFloat($trawdata["SharesOutstandingDiluted"]) * 1000000 . ",";
-                        $query1 .= $trawdata["TotalRevenue"] . ",";
-                        $x1 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? (($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"])/$trawdata["TotalAssets"]) : 'null');
-                        $x2 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["RetainedEarnings"]/$trawdata["TotalAssets"]) : 'null');
-                        $x3 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["EBIT"]/$trawdata["TotalAssets"]) : 'null');
-                        $x5 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["TotalRevenue"]/$trawdata["TotalAssets"]) : 'null');
-                        $query1 .= $x1 . "," . $x2 . "," . $x3 . "," . $x5;
-                        $query1 .= ")";
-                        mysql_query($query1) or die (mysql_error());
-                        //Update MRQ Data
-
-                        $tquery = "SELECT * FROM `reports_header` a, reports_incomeconsolidated b, reports_balanceconsolidated c, reports_gf_data d WHERE a.id=b.report_id AND a.id=c.report_id AND a.id=d.report_id AND a.ticker_id= " . $ppid . " AND report_type='QTR' order by fiscal_year desc, fiscal_quarter desc limit 1";
-                        $tres = mysql_query($tquery) or die (mysql_error());
-                        $trawdata = mysql_fetch_assoc($tres);
-                        array_walk_recursive($trawdata, 'nullValues');
-                        $query1 = "INSERT INTO `mrq_alt_checks` (`ticker_id`, `WorkingCapital`, `TotalAssets`, `TotalLiabilities`, `RetainedEarnings`, `EBIT`, `NetSales`, `X1`, `X2`, `X3`, `X5`) VALUES (";
-                        $query1 .= "'".$ppid."',";
-
-                        $query1 .= ($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"]) . ",";
-                        $query1 .= $trawdata["TotalAssets"] . ",";
-                        $query1 .= $trawdata["TotalLiabilities"] . ",";
-                        $query1 .= $trawdata["RetainedEarnings"] . ",";
-                        $query1 .= $trawdata["EBIT"] . ",";
-                        $query1 .= $trawdata["TotalRevenue"] . ",";
-                        $x1 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? (($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"])/$trawdata["TotalAssets"]) : 'null');
-                        $x2 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["RetainedEarnings"]/$trawdata["TotalAssets"]) : 'null');
-                        $x3 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["EBIT"]/$trawdata["TotalAssets"]) : 'null');
-                        $x5 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["TotalRevenue"]/$trawdata["TotalAssets"]) : 'null');
-                        $query1 .= $x1 . "," . $x2 . "," . $x3 . "," . $x5;
-                        $query1 .= ")";
-                        mysql_query($query1) or die ($query1.mysql_error());
+			altmanTTM($ppid);
                 }
         }
+	altmanTTM($pid);
+}
+
+function pioTTM($ppid,$prawdata,$querypre,$pprawdata) {
+        $queryqtr = "SELECT * FROM reports_header where report_type='QTR' and ticker_id = $ppid order by fiscal_year desc, fiscal_quarter desc limit 1";
+        $resqtr = mysql_query($queryqtr) or die (mysql_error());
+        $rowqtr =  mysql_fetch_assoc($resqtr);
+        if ($rowqtr["fiscal_year"] == $prawdata["fiscal_year"] && $rowqtr["fiscal_quarter"] == $prawdata["fiscal_quarter"]) {
+                $query1 = "INSERT INTO `ttm_pio_checks` (`ticker_id`, `pio1`, `pio2`, `pio3`, `pio4`, `pio5`, `pio6`, `pio7`, `pio8`, `pio9`, `pioTotal`) VALUES (";
+                $query1 .= "'".$ppid."',";
+                mysql_query($query1.$querypre) or die (mysql_error());
+        } else {
+                $total = 0;
+
+                $tquery = "SELECT * FROM `ttm_balanceconsolidated` a, ttm_balancefull b, ttm_cashflowconsolidated c, ttm_cashflowfull d, ttm_financialscustom e, ttm_incomeconsolidated f, ttm_incomefull g, ttm_gf_data h WHERE a.ticker_id=b.ticker_id AND a.ticker_id=c.ticker_id AND a.ticker_id=d.ticker_id AND a.ticker_id=e.ticker_id AND a.ticker_id=f.ticker_id AND a.ticker_id=g.ticker_id and a.ticker_id=h.ticker_id and a.ticker_id = $ppid";
+                $tres = mysql_query($tquery) or die (mysql_error());
+                $trawdata = mysql_fetch_assoc($tres);
+                $query1 = "INSERT INTO `ttm_pio_checks` (`ticker_id`, `pio1`, `pio2`, `pio3`, `pio4`, `pio5`, `pio6`, `pio7`, `pio8`, `pio9`, `pioTotal`) VALUES (";
+                $query1 .= "'".$ppid."',";
+                //Pio 1
+                $value = (!is_null($trawdata["IncomebeforeExtraordinaryItems"]) && $trawdata["IncomebeforeExtraordinaryItems"] >= 0 ? 1 : 0);
+                $total += $value;
+                $query = "'".($value)."',";
+                //Pio 2
+                $value = (!is_null($trawdata["CashfromOperatingActivities"]) && $trawdata["CashfromOperatingActivities"] >= 0 ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 3
+                $vn = (is_null($trawdata["TotalAssets"]) || $trawdata["TotalAssets"] == 0) ? 0 : ($trawdata["IncomebeforeExtraordinaryItems"]/$trawdata["TotalAssets"]);
+                $vv = (is_null($prawdata["TotalAssets"]) || $prawdata["TotalAssets"] == 0) ? 0 : ($prawdata["IncomebeforeExtraordinaryItems"]/$prawdata["TotalAssets"]);
+                $value = ($vn >= $vv ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 4
+                $value = ($trawdata["CashfromOperatingActivities"] >= $trawdata["IncomebeforeExtraordinaryItems"] ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 5
+                $vn = (($trawdata["TotalAssets"]+$prawdata["TotalAssets"]) == 0) ? 0 : (($trawdata["TotalLongtermDebt"])/(($trawdata["TotalAssets"]+$prawdata["TotalAssets"])/2));
+                $vv = (($prawdata["TotalAssets"]+$pprawdata["TotalAssets"]) == 0) ? 0 : (($prawdata["TotalLongtermDebt"])/(($prawdata["TotalAssets"]+$pprawdata["TotalAssets"])/2));
+                $value = ($vn <= $vv ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 6
+                $vn = (is_null($trawdata["TotalCurrentLiabilities"]) || $trawdata["TotalCurrentLiabilities"] == 0) ? 0 : ($trawdata["TotalCurrentAssets"]/$trawdata["TotalCurrentLiabilities"]);
+                $vv = (is_null($prawdata["TotalCurrentLiabilities"]) || $prawdata["TotalCurrentLiabilities"] == 0) ? 0 : ($prawdata["TotalCurrentAssets"]/$prawdata["TotalCurrentLiabilities"]);
+                $value = ($vn >= $vv ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 7
+                $value = (toFloat($trawdata["SharesOutstandingDiluted"]) <= toFloat($prawdata["SharesOutstandingDiluted"]) ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 8
+                $vn = (is_null($trawdata["TotalRevenue"]) || $trawdata["TotalRevenue"] == 0) ? 0 : ($trawdata["GrossProfit"]/$trawdata["TotalRevenue"]);
+                $vv = (is_null($prawdata["TotalRevenue"]) || $prawdata["TotalRevenue"] == 0) ? 0 : ($prawdata["GrossProfit"]/$prawdata["TotalRevenue"]);
+                $value = ($vn >= $vv ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                //Pio 9
+                $vn = (is_null($prawdata["TotalAssets"]) || $prawdata["TotalAssets"] == 0) ? 0 : ($trawdata["TotalRevenue"]/$prawdata["TotalAssets"]);
+                $vv = (is_null($pprawdata["TotalAssets"]) || $pprawdata["TotalAssets"] == 0) ? 0 : ($prawdata["TotalRevenue"]/$pprawdata["TotalAssets"]);
+                $value = ($vn >= $vv ? 1 : 0);
+                $total += $value;
+                $query .= "'".($value)."',";
+                $query .= "'".($total)."'";
+                $query .= ")";
+                mysql_query($query1.$query) or die (mysql_error());
+	}
+}
+
+function altmanTTM($ppid) {
+        $tquery = "SELECT * FROM ttm_incomeconsolidated a, ttm_balanceconsolidated b, ttm_gf_data c WHERE a.ticker_id=b.ticker_id AND a.ticker_id=c.ticker_id AND a.ticker_id= " . $ppid;
+        $tres = mysql_query($tquery) or die (mysql_error());
+        $trawdata = mysql_fetch_assoc($tres);
+        array_walk_recursive($trawdata, 'nullValues');
+        $query1 = "INSERT INTO `ttm_alt_checks` (`ticker_id`, `WorkingCapital`, `TotalAssets`, `TotalLiabilities`, `RetainedEarnings`, `EBIT`, `SharesOutstandingDiluted`, `NetSales`, `X1`, `X2`, `X3`, `X5`) VALUES (";
+        $query1 .= "'".$ppid."',";
+
+        $query1 .= ($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"]) . ",";
+        $query1 .= $trawdata["TotalAssets"] . ",";
+        $query1 .= $trawdata["TotalLiabilities"] . ",";
+        $query1 .= $trawdata["RetainedEarnings"] . ",";
+        $query1 .= $trawdata["EBIT"] . ",";
+        $query1 .= toFloat($trawdata["SharesOutstandingDiluted"]) * 1000000 . ",";
+        $query1 .= $trawdata["TotalRevenue"] . ",";
+        $x1 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? (($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"])/$trawdata["TotalAssets"]) : 'null');
+        $x2 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["RetainedEarnings"]/$trawdata["TotalAssets"]) : 'null');
+        $x3 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["EBIT"]/$trawdata["TotalAssets"]) : 'null');
+        $x5 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["TotalRevenue"]/$trawdata["TotalAssets"]) : 'null');
+        $query1 .= $x1 . "," . $x2 . "," . $x3 . "," . $x5;
+        $query1 .= ")";
+        mysql_query($query1) or die (mysql_error());
+        //Update MRQ Data
+
+        $tquery = "SELECT * FROM `reports_header` a, reports_incomeconsolidated b, reports_balanceconsolidated c, reports_gf_data d WHERE a.id=b.report_id AND a.id=c.report_id AND a.id=d.report_id AND a.ticker_id= " . $ppid . " AND report_type='QTR' order by fiscal_year desc, fiscal_quarter desc limit 1";
+        $tres = mysql_query($tquery) or die (mysql_error());
+        $trawdata = mysql_fetch_assoc($tres);
+        array_walk_recursive($trawdata, 'nullValues');
+        $query1 = "INSERT INTO `mrq_alt_checks` (`ticker_id`, `WorkingCapital`, `TotalAssets`, `TotalLiabilities`, `RetainedEarnings`, `EBIT`, `NetSales`, `X1`, `X2`, `X3`, `X5`) VALUES (";
+        $query1 .= "'".$ppid."',";
+        $query1 .= ($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"]) . ",";
+        $query1 .= $trawdata["TotalAssets"] . ",";
+        $query1 .= $trawdata["TotalLiabilities"] . ",";
+        $query1 .= $trawdata["RetainedEarnings"] . ",";
+        $query1 .= $trawdata["EBIT"] . ",";
+        $query1 .= $trawdata["TotalRevenue"] . ",";
+        $x1 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? (($trawdata["TotalCurrentAssets"] - $trawdata["TotalCurrentLiabilities"])/$trawdata["TotalAssets"]) : 'null');
+        $x2 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["RetainedEarnings"]/$trawdata["TotalAssets"]) : 'null');
+        $x3 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["EBIT"]/$trawdata["TotalAssets"]) : 'null');
+        $x5 = ($trawdata["TotalAssets"] !== 'null' && $trawdata["TotalAssets"] != 0 ? ($trawdata["TotalRevenue"]/$trawdata["TotalAssets"]) : 'null');
+        $query1 .= $x1 . "," . $x2 . "," . $x3 . "," . $x5;
+        $query1 .= ")";
+        mysql_query($query1) or die ($query1.mysql_error());
 }
 ?>
