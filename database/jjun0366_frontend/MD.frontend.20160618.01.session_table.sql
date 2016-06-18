5984192b6a6382e53f39955e54a75ba0eecb0784
Sergio <skarsvnie@gmail.com>
2016-06-18 06:49:16 -0400

    Add index to session table

--- database/jjun0366_frontend/MD.frontend.20160618.01.session_table.sql
+++ database/jjun0366_frontend/MD.frontend.20160618.01.session_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `sessions` (
+CREATE TABLE IF NOT EXISTS `sessions` (
   `id` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
-  `username` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin,
+  `username` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `last_activity` int(10) unsigned DEFAULT NULL,
   `expiration` int(10) unsigned DEFAULT NULL,
   `data` text CHARACTER SET utf8 COLLATE utf8_bin,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
