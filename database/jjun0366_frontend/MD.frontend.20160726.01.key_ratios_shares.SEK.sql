b1811f42f2ff72ec1619d8641c9dde89aa1dcf41
Sergio <skarsvnie@gmail.com>
2016-07-26 18:09:39 -0400

    OSA-551 [DATA] middleware function not displaying data on frontend

---
+++ database/jjun0366_frontend/MD.frontend.20160726.01.key_ratios_shares.SEK.sql
@@ -0,0 +1,7 @@
+ALTER TABLE  `reports_key_ratios` DROP  `SharesOutstandingDiluted` ;
+ALTER TABLE  `reports_key_ratios_3cagr` DROP  `SharesOutstandingDiluted` ;
+ALTER TABLE  `reports_key_ratios_5cagr` DROP  `SharesOutstandingDiluted` ;
+ALTER TABLE  `reports_key_ratios_7cagr` DROP  `SharesOutstandingDiluted` ;
+ALTER TABLE  `reports_key_ratios_10cagr` DROP  `SharesOutstandingDiluted` ;
+ALTER TABLE  `ttm_key_ratios` DROP  `SharesOutstandingDiluted` ;
+
