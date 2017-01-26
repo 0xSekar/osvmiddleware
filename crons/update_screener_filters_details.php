f5bee990a937480820db199b963306a809680a31
Sergio <skarsvnie@gmail.com>
2017-01-26 17:41:12 -0500

    OSA-822 Allow dynamic add/remove filters

--- crons/update_screener_filters_details.php
+++ crons/update_screener_filters_details.php
@@ -2,6 +2,6 @@
 require_once(dirname(__FILE__)."/include/screener_filter_class.php");
 
 $ti = new screener_filter();
-$ti->updateCommentsDescriptions();
+$ti->fullFiltersReplace();
 
-echo "Filters names and descriptions updated";
+echo "Filters updated\n";
