64f829b1de7df1735258581315674d228d7599e3
Sergio <skarsvnie@gmail.com>
2017-03-08 12:46:12 -0500

    OSA-960 update screener table name in database to prevent confusion

---
+++ database/jjun0366_frontend/MD.frontend.20170308.02.screener_field_X_to_reports.SEK.sql
@@ -0,0 +1,3 @@
+RENAME TABLE  screener_field_fields` TO  `screener_reports_fields` ;
+RENAME TABLE  `screener_field_groups` TO `screener_reports_groups` ;
+RENAME TABLE  `screener_field_persistent` TO  `screener_reports_persistent` ;
