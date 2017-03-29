ca82cd186c528cd39ebbb1ec157f9b5359eb4712
Nguyen The Tuyen <nttuyen_it@yahoo.com>
2017-03-29 11:18:35 +0700

    OSA-1080: update query to create table chart_export

--- database/jjun0366_frontend/MD.frontend.20170315.01.chart_export.SEK.sql
+++ database/jjun0366_frontend/MD.frontend.20170315.01.chart_export.SEK.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `chart_export` (
   `id` char(32) NOT NULL,
+  `ticker` varchar(20) NOT NULL,
   `chart_options` longtext NOT NULL,
   `exported_date` varchar(45) NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `checksum_UNIQUE` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
