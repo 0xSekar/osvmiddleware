748a755d45e5bde4b4e3cabaf4fcb59cd9fef387
Nguyen The Tuyen <nttuyen_it@yahoo.com>
2017-08-05 18:16:59 +0700

    OSA-1393 [ONBOARDING] main task issue to track onboarding related tasks

---
+++ database/jjun0366_frontend/MD.frontend.20170805.01.tutorial_video.NTT.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tutorial_video` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `video_id` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `page` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
