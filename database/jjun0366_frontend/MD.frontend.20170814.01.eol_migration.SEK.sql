ce6e2a577ddee040af9d5732ded52783749b97d7
Sergio <skarsvnie@gmail.com>
2017-08-14 20:15:03 -0400

    OSA-935 Change split_partser table field

--- database/jjun0366_frontend/MD.frontend.20170814.01.eol_migration.SEK.sql
+++ database/jjun0366_frontend/MD.frontend.20170814.01.eol_migration.SEK.sql
@@ -3,3 +3,6 @@ ALTER TABLE  `tickers_proedgard_updates` ADD  `missing_gf_period` DATE NULL ;
 ALTER TABLE  `tickers_proedgard_updates` CHANGE  `filed_date`  `filed_date` DATETIME NULL DEFAULT NULL COMMENT 'Filed date, taken from email subject alert',
 CHANGE  `tested_for_today`  `tested_for_today` DATETIME NULL DEFAULT NULL COMMENT 'To check if today this record has been tested or not, if yes, then system should ignore',
 CHANGE  `missing_gf_period`  `missing_gf_period` DATETIME NULL DEFAULT NULL ;
+
+ALTER TABLE  `tickers_split_parser` CHANGE  `tested_for_today`  `tested_for_today` DATETIME NULL DEFAULT NULL ;
+
