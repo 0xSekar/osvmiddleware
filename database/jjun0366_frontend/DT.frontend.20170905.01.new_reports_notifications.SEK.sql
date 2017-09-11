af0eb4fd828a1345f68b5bdbb2aa848ce0040c89
Sergio <skarsvnie@gmail.com>
2017-09-11 10:37:54 -0400

    OSA-1012 Notifications of stocks that had data updated

---
+++ database/jjun0366_frontend/DT.frontend.20170905.01.new_reports_notifications.SEK.sql
@@ -0,0 +1,6 @@
+INSERT INTO  `fields_metadata` (`metadata_id` ,`table_name` ,`field_name` ,`title` ,`short_title` ,`format` ,`min` ,`max` ,`table_group` ,`field_group` ,`tooltip` ,`field_type` ,`good_increase` ,`field_order`)
+VALUES (NULL ,  'reports_header',  'report_date',  'Report Date',  'Report Date',  'osvdate', NULL , NULL ,  '2', NULL ,  'Report Date',  'D', NULL ,  '');
+INSERT INTO  `fields_metadata` (`metadata_id` ,`table_name` ,`field_name` ,`title` ,`short_title` ,`format` ,`min` ,`max` ,`table_group` ,`field_group` ,`tooltip` ,`field_type` ,`good_increase` ,`field_order`)
+VALUES (NULL ,  'reports_header',  'fiscal_quarter',  'Fiscal qtr',  'Fiscal Quarter',  'osvnumber:0', NULL , NULL ,  '2', NULL ,  'Report Fiscal Quarter',  'N', NULL ,  '0');
+INSERT INTO  `fields_metadata` (`metadata_id` ,`table_name` ,`field_name` ,`title` ,`short_title` ,`format` ,`min` ,`max` ,`table_group` ,`field_group` ,`tooltip` ,`field_type` ,`good_increase` ,`field_order`)
+VALUES (NULL ,  'reports_financialheader',  'FormType',  'Form Type',  'Form Type',  'osvstring', NULL , NULL ,  '2', NULL ,  'Report Filling Form Type',  'S', NULL ,  '0');
