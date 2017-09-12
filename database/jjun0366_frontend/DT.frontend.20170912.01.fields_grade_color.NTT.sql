1799a28057698c709ed177496510fa10d92f4fe5
Nguyen The Tuyen <nttuyen_it@yahoo.com>
2017-09-12 17:47:38 +0700

    OSA-1496: [QUALITY CHECKS DASHBOARD]

---
+++ database/jjun0366_frontend/DT.frontend.20170912.01.fields_grade_color.NTT.sql
@@ -0,0 +1,10 @@
+UPDATE `fields_grade_color` SET `summary`='Safe' WHERE `id`='132';
+UPDATE `fields_grade_color` SET `summary`='Manipulator' WHERE `id`='131';
+
+UPDATE `fields_grade_color` SET `summary`='Danger' WHERE `id`='165';
+UPDATE `fields_grade_color` SET `summary`='Caution' WHERE `id`='164';
+UPDATE `fields_grade_color` SET `summary`='Safe' WHERE `id`='163';
+
+UPDATE `fields_grade_color` SET `summary`='Danger' WHERE `id`='177';
+UPDATE `fields_grade_color` SET `summary`='Fair' WHERE `id`='176';
+UPDATE `fields_grade_color` SET `summary`='Good' WHERE `id`='175';
