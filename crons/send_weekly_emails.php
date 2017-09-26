3316042ebfce9ac355558ab5bff3360511d7cb7c
Sergio <skarsvnie@gmail.com>
2017-09-26 09:45:32 -0400

    OSA-482 [EMAIL] Weekly OSV Rating Changes

---
+++ crons/send_weekly_emails.php
@@ -0,0 +1,256 @@
+<?php
+// Database Connection
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+include_once('../config.php');
+include_once('../db/db.php');
+
+set_time_limit(0);                   // ignore php timeout
+//ignore_user_abort(true);             // keep on going even if user pulls the plug*
+while(ob_get_level())ob_end_clean(); // remove output buffers
+ob_implicit_flush(true);             // output stuff directly
+
+// Should never be cached - do not remove this
+header("Content-type: text/xml; charset=utf-8");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+$reference_date = date('Y-m-d', strtotime('-7 days'));
+$year = date('Y');
+$count = 0;
+
+//Get Valid users
+$user_list = getUserList();
+
+foreach ($user_list as $user) {
+    //Get relevant Tickers
+    $stocks = getUserStocks($user);
+
+    //Get Stocks arrays
+    $upStocks = getUpTickers($stocks, $reference_date, $year, 10); //New A Grade stocks
+    $downStocks = getDownTickers($stocks, $reference_date, $year, 10); //Downgraded from A stocks
+    $topTrifecta = getActionWidget($year, $sort = "action", $sorttype = "desc", $number = 5, $filter = false, $mc = 500, $qt = 85, $vt = 85, $gt = 85);
+    $topAction = getActionWidget($year, $sort = "action", $sorttype = "desc", $number = 5, $filter = false, $mc = 500);
+
+    //Send Email
+    $content = getContent("templates/email.php", $upStocks, $downStocks, $topTrifecta, $topAction);
+    $headers[] = 'From: "Jae Jun" <osv@oldschoolvalue.com>';
+    $headers[] = 'Reply-To: "Jae Jun" <osv@oldschoolvalue.com>';
+    $headers[] = 'MIME-Version: 1.0\r\n';
+    $headers[] = 'Content-Type: text/html; charset=UTF-8';
+    $subject = "Weekly Old School Value Rating Update";
+    mail($user, $subject, $content, implode( "\r\n" , $headers ));
+    echo "Email sent to $user<br>\n";
+    $count++;
+}
+echo "<br>\n$count mails sent<br>\n";
+
+function getUserList() {
+    $db = Database::GetInstance();
+
+    $query = $db->prepare("SELECT DISTINCT user_id FROM `user_persistent_data` u Where NOT Exists (SELECT * from user_persistent_data t WHERE t.user_id=u.user_id AND t.name='email_weekly')
+            UNION SELECT DISTINCT user_id FROM `user_persistent_data` u Where u.name='email_weekly' AND u.value = ?");
+    $query->execute(array('"1"'));
+    return $query->fetchAll(PDO::FETCH_COLUMN);
+}
+
+function getUserStocks($user) {
+    $db = Database::GetInstance();
+
+    $stocks = array();
+    $stocks1 = null;
+    $query = $db->prepare("SELECT value FROM user_persistent_data WHERE user_id = ? AND name = 'favorite_stocks'");
+    $query->execute(array($user));
+    if ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $stocks1 =  json_decode($row["value"]);
+    }
+    $query = "SELECT distinct ticker FROM `portfolio_persistent` a inner join portfolio_stocks b on a.id=b.portfolio_id inner join tickers c on b.ticker_id = c.id WHERE user_id = ?";
+    $res = $db->prepare($query);
+    $res->execute(array($user));
+    $stocks2 = $res->fetchAll(PDO::FETCH_COLUMN);
+    $stocks = array_values(array_unique(array_merge($stocks1, $stocks2)));
+    return $stocks;
+}
+
+function getUpTickers($stocks, $reference_date, $year, $limit) {
+    $db = Database::GetInstance();
+
+    $result = array();
+    $query = "SELECT t.id, t.ticker, t.company, r.AS AS c_AS, h.AS AS o_AS, k.MarketCapIntraday, ((q.LastTradePriceOnly - qh.adj_close) / qh.adj_close * 100) AS YTD, h2.ratings_date as c_date
+        FROM tickers t
+        INNER JOIN ttm_ratings r ON t.id = r.ticker_id
+        INNER JOIN ttm_ratings_history h ON t.id = h.ticker_id
+        LEFT JOIN tickers_yahoo_keystats_1 k ON t.id = k.ticker_id
+        LEFT JOIN tickers_yahoo_quotes_2 q ON t.id = q.ticker_id
+        LEFT JOIN tickers_yahoo_historical_data qh ON t.id = qh.ticker_id
+        INNER JOIN ttm_ratings_history h2 ON t.id = h2.ticker_id
+        WHERE h.ratings_date = '$reference_date'
+        AND r.AS_grade =  'A'
+        AND h.AS_grade !=  'A'
+        AND t.ticker IN ('".implode("','", $stocks)."')
+        AND qh.report_date = (SELECT MAX(report_date) from tickers_yahoo_historical_data te where te.ticker_id=t.id and te.report_date < '$year-01-01')
+        AND h2.ratings_date = (SELECT MIN(h3.ratings_date) from ttm_ratings_history h3 where h3.ticker_id=t.id and h3.ratings_date <= now() AND h3.ratings_date >= '$reference_date' AND h3.AS_grade = 'A') ORDER BY r.AS DESC LIMIT $limit";
+    $res = $db->prepare($query);
+    $res->execute();
+    $res1 = $res->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($res1 as $value) {
+        $result[$value["ticker"]] = $value;
+    }
+    return $result;
+}
+
+function getDownTickers($stocks, $reference_date, $year, $limit) {
+    $db = Database::GetInstance();
+
+    $result = array();
+    $query = "SELECT t.id, t.ticker, t.company, r.AS AS c_AS, h.AS AS o_AS, k.MarketCapIntraday, ((q.LastTradePriceOnly - qh.adj_close) / qh.adj_close * 100) AS YTD, h2.ratings_date as c_date
+        FROM tickers t
+        INNER JOIN ttm_ratings r ON t.id = r.ticker_id
+        INNER JOIN ttm_ratings_history h ON t.id = h.ticker_id
+        LEFT JOIN tickers_yahoo_keystats_1 k ON t.id = k.ticker_id
+        LEFT JOIN tickers_yahoo_quotes_2 q ON t.id = q.ticker_id
+        LEFT JOIN tickers_yahoo_historical_data qh ON t.id = qh.ticker_id
+        INNER JOIN ttm_ratings_history h2 ON t.id = h2.ticker_id
+        WHERE h.ratings_date = '$reference_date'
+        AND r.AS_grade !=  'A'
+        AND h.AS_grade =  'A'
+        AND t.ticker IN ('".implode("','", $stocks)."')
+        AND qh.report_date = (SELECT MAX(report_date) from tickers_yahoo_historical_data te where te.ticker_id=t.id and te.report_date < '$year-01-01')
+        AND h2.ratings_date = (SELECT MIN(h3.ratings_date) from ttm_ratings_history h3 where h3.ticker_id=t.id and h3.ratings_date <= now() AND h3.ratings_date >= '$reference_date' AND h3.AS_grade != 'A') ORDER BY r.AS DESC LIMIT $limit";
+    $res = $db->prepare($query);
+    $res->execute();
+    $res1 = $res->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($res1 as $value) {
+        $result[$value["ticker"]] = $value;
+    }
+    return $result;
+}
+
+function getActionWidget($year, $sort = "action", $sorttype = "desc", $number = 10, $filter = false, $mc = null, $qt = null, $vt = null, $gt = null, $as = null) {
+    $db = Database::GetInstance();
+
+    $result = array();
+    if (!is_numeric($number) || (!is_null($mc) && !is_numeric($mc)) || (!is_null($qt) && !is_numeric($qt)) || (!is_null($vt) && !is_numeric($vt)) || (!is_null($gt) && !is_numeric($gt)) || (!is_null($as) && !is_numeric($as))) {
+        return $result;
+    }
+    $query = "SELECT t.ticker, t.company, k.MarketCapIntraday, ((q.LastTradePriceOnly - qh.adj_close) / qh.adj_close * 100) AS YTD, r.QT, r.VT, r.GT, r.AS, r.AS_grade FROM ttm_ratings r LEFT JOIN tickers t on r.ticker_id=t.id LEFT JOIN tickers_yahoo_keystats_1 k ON t.id=k.ticker_id LEFT JOIN tickers_yahoo_quotes_2 q ON t.id = q.ticker_id LEFT JOIN tickers_yahoo_historical_data qh ON t.id = qh.ticker_id";
+    $query .= " WHERE t.is_old = FALSE AND qh.report_date = (SELECT MAX(report_date) from tickers_yahoo_historical_data te where te.ticker_id=t.id and te.report_date < '$year-01-01')";
+    if (!is_null($mc)) {
+        $mc = $mc * 1000000;
+        $query .= " AND k.MarketCapIntraday > :mc";
+    }
+    if($filter) {
+        $query .= " AND ";
+        $query .= "t.sector != 'Financial' AND t.sector != 'Basic Materials' AND t.sector !='Utilities' AND t.exchange != 'OTC'";
+    }
+    if(!is_null($qt)) {
+        $query .= " AND ";
+        $query .= "r.QT >= :qt";
+    }
+    if(!is_null($vt)) {
+        $query .= " AND ";
+        $query .= "r.VT >= :vt";
+    }
+    if(!is_null($gt)) {
+        $query .= " AND ";
+        $query .= "r.GT >= :gt";
+    }
+    if(!is_null($as)) {
+        $query .= " AND ";
+        $query .= "r.AS >= :as";
+    }
+    $query .= " ORDER BY ";
+    switch(strtolower($sort)) {
+        case "growth":
+            $query .= "r.GT";
+            break;
+        case "value":
+            $query .= "r.VT";
+            break;
+        case "quality":
+            $query .= "r.QT";
+            break;
+        default:
+            $query .= "r.AS";
+    }
+    if(strtolower($sorttype) == "asc")
+        $query .= " ASC";
+    else
+        $query .= " DESC";
+    if(!$filter) {
+        $query .= " LIMIT :limit";
+    }
+    $res = $db->prepare($query);
+    if (!is_null($mc))
+        $res->bindParam(':mc', $mc, PDO::PARAM_INT);
+    if (!$filter)
+        $res->bindParam(':limit', $number, PDO::PARAM_INT);
+    if (!is_null($gt))
+        $res->bindParam(':gt', $gt, PDO::PARAM_STR);
+    if (!is_null($vt))
+        $res->bindParam(':vt', $vt, PDO::PARAM_STR);
+    if (!is_null($qt))
+        $res->bindParam(':qt', $qt, PDO::PARAM_STR);
+    if (!is_null($as))
+        $res->bindParam(':as', $as, PDO::PARAM_STR);
+    $res->execute();
+    $count = 0;
+    $tickers = array();
+    while(($row = $res->fetch(PDO::FETCH_ASSOC)) && $count < $number) {
+        $count++;
+        $tickers[] = $row;
+    }
+    $result = array();
+    if(count($tickers) > 0) {
+        foreach($tickers as $symbol) {
+            $result[$symbol["ticker"]] = $symbol;
+        }
+    }
+    return $result;
+}
+
+function getContent($file = "templates/email.php", $upStocks = array(), $downStocks = array(), $topTrifecta = array(), $topAction= array()) {
+    ob_start(); // start output buffer
+
+    include $file;
+    $template = ob_get_contents(); // get contents of buffer
+    ob_end_clean();
+    return $template;
+}
+
+function cutValue($value, $max, $tail) {
+    if (!$value) return '';
+
+    $totalMax = $max + ($tail ? strlen($tail) : 0);
+    if (!$max) return $value;
+    if (strlen($value) <= $totalMax) return $value;
+
+    $value = substr($value,0, $max);
+
+    return trim($value) . ($tail ? $tail : 'â~@¦');
+}
+
+function formatCurrency($value) {
+    if(!is_numeric($value)) {
+        return "N/A";
+    }
+    $suffix = '';
+    if($value > 1000000000) {
+        $value = $value / 1000000000;
+        $suffix = 'B';
+    } else if($value > 1000000) {
+        $value = $value / 1000000;
+        $suffix = 'M';
+    } else if($value > 1000) {
+        $value = $value / 1000;
+        $suffix = 'K';
+    }
+    $formatted = number_format($value, 2, '.', '');
+    while (strpos($formatted, '.') !== false && substr($formatted,-1) == '0') {
+        $formatted = substr($formatted, 0, -1);
+    }
+    if(substr($formatted,-1) == '.') {
+        $formatted = substr($formatted, 0, -1);
+    }
+    return '$'.$formatted.$suffix;
+}
+?>
---
+++ crons/templates/email.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Email-OSV</title>
+</head>
+
+<body style="background:#F6F7F9; overflow: auto;">
+<?php
+    $styleh3="float:left; margin-bottom:-5px;margin-top: 15px;color: #54657e;";
+    $container="width:658px; margin: 0 auto; background: #fff;padding: 15px;border: 1px solid #c0c0c0; overflow: auto;";
+    $logo="float: left;background: #262e40; width: 100%;height: 70px; ";
+    $title="background:#343f56; height:30px; width:100%; margin-top: 25px; float:left;color: #fefefe;";
+    $stylep="float:left;color: #262624; margin-bottom: 10px;";
+    $styleh4="padding-left:10px; margin-top: 7px;";
+    $table=" margin-top:10px; width: 100%; max-width: 100%;border-collapse: collapse;";
+    $styletr="border-bottom: 1px solid #ddd;";
+    $td150="width:150px;padding: 7px 0;Color:#54657e;";
+    $td100="width:100px;padding: 7px 0;text-align:center;Color:#54657e;";
+    $td50="width:50px;text-align:center; Color:#54657e;font-size:14px;padding: 7px 0;";
+    $td200="width:200px;padding: 7px 0;Color:#54657e;";
+    $tdName150="width:150px;color: #54657E;padding: 7px 0;";
+    $tdName200="width:200px;color: #54657E;padding-left:10px";
+    $td="vertical-align: middle;line-height: 15px; color:#b1b4ba;";
+    $spanUp="vertical-align:middle;display: inline-block;  border-right: 4px solid transparent;   border-left: 4px solid transparent;   border-top: 0;border-bottom: 4px dashed;margin-right: 4px;";
+    $spanDown="vertical-align:middle;display: inline-block;  margin-right: 4px;  vertical-align: middle;  border-top: 4px dashed; border-top: 4px solid \9;   border-right: 4px solid transparent;   border-left: 4px solid transparent;";
+?>
+<div class = "container" style="<?php echo $container ?>">
+    <div class="logo" style="<?php echo $logo ?>">
+        <img alt="old school value" src="http://cdn8.oldschoolvalue.com/images/oldschoolvalue-logo.png" style="padding: 18px">
+    </div>
+    <div class="title" style="<?php echo $title?>">
+        <h4 style="<?php echo $styleh4?>"><b>OLD SCHOOL VALUE</b> - <?php echo date('l, F j, Y');?></h4>
+    </div>
+    <h3 style="<?php echo $styleh3?>">New "A" Grade Action Score Stocks (Portfolio and Favorite Stocks)</h3>
+    <p style="<?php echo $stylep ?>">The following stocks have entered the A list. This list is currently limited to 10 stocks. To see the full list, please <a href="https://app.oldschoovalue.com">log in</a> and go to "Stock Database" to see the full list.</p>
+    <div class= "table1" style="float:left">
+        <table style="<?php echo $table?>">
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>;color:#b1b4ba;">
+                    Name
+                </td >
+                <td style="<?php echo $td100,$td?>">
+                    Market Cap
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    YTD %Chg
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    New Action Score
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    Old Action Score
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    Date Changed
+                </td>
+            </tr>
+            <?php if (count($upStocks) == 0) {?>
+            <tr style="<?php echo $styletr?>">
+                <td colspan="6" style="<?php echo $td100?>;width:100%;">
+                    There is no stocks matching this criteria
+                </td >
+            </tr>
+            <?php } else {?>
+            <?php foreach($upStocks as $stock) {?>
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>">
+                    <?php echo cutValue($stock["company"], 12)." (".$stock["ticker"].")";?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo formatCurrency($stock["MarketCapIntraday"]);?>
+                </td>
+                <td style="<?php echo $td100; echo $stock["YTD"] < 0 ? ' color:red;' : ' color:green;';?> font-size: 14px;">
+                    <span class="caret" style="<?php echo $stock["YTD"] < 0 ? $spanDown : $spanUp;?>"></span><?php echo number_format($stock["YTD"], 2, '.', '').'%';?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo number_format($stock["c_AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo number_format($stock["o_AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td100?>;font-size: 14px;">
+                    <?php echo date("n/j/Y",strtotime($stock["c_date"]));?>
+                </td>
+            </tr>
+            <?php }?>
+            <?php }?>
+        </table>
+        <h3 style="<?php echo $styleh3?>">Downgraded Action Score Stocks (Portfolio and Favorite Stocks)</h3>
+        <p style="<?php echo $stylep ?>">The following stocks have left the A list. This list is currently limited to 10 stocks. To see the full list, please <a href="https://app.oldschoovalue.com">log in</a> and go to "Stock Database" to see the full list.</p>
+        <table style="<?php echo $table?>">
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>;color:#b1b4ba;">
+                    Name
+                </td >
+                <td style="<?php echo $td100,$td?>">
+                    Market Cap
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    YTD %Chg
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    New Action Score
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    Old Action Score
+                </td>
+                <td style="<?php echo $td100,$td?>">
+                    Date Changed
+                </td>
+            </tr>
+            <?php if (count($downStocks) == 0) {?>
+            <tr style="<?php echo $styletr?>">
+                <td colspan="6" style="<?php echo $td100?>;width:100%;">
+                    There is no stocks matching this criteria
+                </td >
+            </tr>
+            <?php } else {?>
+            <?php foreach($downStocks as $stock) {?>
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>">
+                    <?php echo cutValue($stock["company"], 12)." (".$stock["ticker"].")";?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo formatCurrency($stock["MarketCapIntraday"]);?>
+                </td>
+                <td style="<?php echo $td100; echo $stock["YTD"] < 0 ? ' color:red;' : ' color:green;';?> font-size: 14px;">
+                    <span class="caret" style="<?php echo $stock["YTD"] < 0 ? $spanDown : $spanUp;?>"></span><?php echo number_format($stock["YTD"], 2, '.', '').'%';?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo number_format($stock["c_AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td100?>">
+                    <?php echo number_format($stock["o_AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td100?>;font-size: 14px;">
+                    <?php echo date("n/j/Y",strtotime($stock["c_date"]));?>
+                </td>
+            </tr>
+            <?php }?>
+            <?php }?>
+        </table>
+        <h3 style="<?php echo $styleh3?>">Top 5 TRIFECTA Stocks (MktCap>$500M)</h3>
+        <p style="<?php echo $stylep?>">A <b>Trifecta</b> represents a stock where Q,V,G and Action is 'A' grade and the score is above 85. This list includes stocks from the full stock universe. Read how the Action Score was created from <a href='http://www.oldschoolvalue.com/blog/valuation-methods/action-score-quality-value-growth/' target='_blank'>this article</a></p>
+        <table style="<?php echo $table?>">
+
+            <tr style="<?php echo $styletr ;$td?>">
+                <td style="<?php echo $tdName200?>;color:#b1b4ba;">
+                    Name
+                </td >
+                <td style="<?php echo $td50,$td?>">
+                    Market Cap
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    YTD %Chg
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Action Grade
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Action Score
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Q
+                <td style="<?php echo $td50,$td?>">
+                    V
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    G
+                </td>
+            </tr>
+            <?php if (count($topTrifecta) == 0) {?>
+            <tr style="<?php echo $styletr?>">
+                <td colspan="8" style="<?php echo $td100?>;width:100%;">
+                    There is no stocks matching this criteria
+                </td>
+            </tr>
+            <?php } else {?>
+            <?php foreach($topTrifecta as $stock) {?>
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>">
+                    <?php echo cutValue($stock["company"], 12)." (".$stock["ticker"].")";?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo formatCurrency($stock["MarketCapIntraday"]);?>
+                </td>
+                <td style="<?php echo $td100; echo $stock["YTD"] < 0 ? ' color:red;' : ' color:green;';?>">
+                    <span class="caret" style="<?php echo $stock["YTD"] < 0 ? $spanDown : $spanUp;?>"></span><?php echo number_format($stock["YTD"], 2, '.', '').'%';?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo $stock["AS_grade"];?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["QT"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["VT"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["GT"], 2, '.', '');?>
+                </td>
+            </tr>
+            <?php }?>
+            <?php }?>
+        </table>
+        <h3 style="<?php echo $styleh3?>">Top 5 ACTION Stocks (MktCap>$500M)</h3>
+        <p style="<?php echo $stylep?>">The Top 5 ACTION Score stocks in our full universe where market cap is > $500m.
+        Stock universe includes financials, basic materials, utilities, OTC and ADR stocks. Read how the Action Score was created from <a href='http://www.oldschoolvalue.com/blog/valuation-methods/action-score-quality-value-growth/' target='_blank'>this article</a></p>
+        <table style="<?php echo $table?>">
+
+            <tr style="<?php echo $styletr ;$td?>">
+                <td style="<?php echo $tdName200?>;color:#b1b4ba;">
+                    Name
+                </td >
+                <td style="<?php echo $td50,$td?>">
+                    Market Cap
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    YTD %Chg
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Action Grade
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Action Score
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    Q
+                <td style="<?php echo $td50,$td?>">
+                    V
+                </td>
+                <td style="<?php echo $td50,$td?>">
+                    G
+                </td>
+            </tr>
+            <?php if (count($topAction) == 0) {?>
+            <tr style="<?php echo $styletr?>">
+                <td colspan="8" style="<?php echo $td100?>;width:100%;">
+                    There is no stocks matching this criteria
+                </td>
+            </tr>
+            <?php } else {?>
+            <?php foreach($topAction as $stock) {?>
+            <tr style="<?php echo $styletr?>">
+                <td style="<?php echo $tdName200?>">
+                    <?php echo cutValue($stock["company"], 12)." (".$stock["ticker"].")";?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo formatCurrency($stock["MarketCapIntraday"]);?>
+                </td>
+                <td style="<?php echo $td100; echo $stock["YTD"] < 0 ? ' color:red;' : ' color:green;';?>">
+                    <span class="caret" style="<?php echo $stock["YTD"] < 0 ? $spanDown : $spanUp;?>"></span><?php echo number_format($stock["YTD"], 2, '.', '').'%';?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo $stock["AS_grade"];?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["AS"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["QT"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["VT"], 2, '.', '');?>
+                </td>
+                <td style="<?php echo $td50?>">
+                    <?php echo number_format($stock["GT"], 2, '.', '');?>
+                </td>
+            </tr>
+            <?php }?>
+            <?php }?>
+        </table>
+    </div>
+    <div style="clear: both"></div>
+</div>
+</body>
+</html>
