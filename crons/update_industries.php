d744d3dd3a1cf78d6faeeffadf404587d1294320
Sergio <skarsvnie@gmail.com>
2017-10-01 10:25:12 -0400

    OSA-996 Mention Jae on industry updates errors

--- crons/update_industries.php
+++ crons/update_industries.php
@@ -3,23 +3,6 @@
 include_once('../config.php');
 include_once('../db/db.php');
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-include_once('./include/raw_data_update_queries.php');
-include_once('./include/update_key_ratios_ttm.php');
-include_once('./include/update_quality_checks.php');
-include_once('./include/update_ratings.php');
-include_once('./include/update_ratings_ttm.php');
-include_once('./include/update_is_old_field.php');
-include_once('./update_frontend_yahoo_daily_include.php');
-include_once('./include/raw_data_update_yahoo_keystats.php');
-require_once("../include/yahoo/common.inc.php");
-include_once('./include/update_eod_valuation.php');
-include_once('./update_frontend_EOL_GF_data_include.php');
-
-// Tools & functions
-include_once('./include/guru.php'); 
-include_once('./include/eol.php'); 
-include_once('./include/eol_xml_parser.php');
-include_once('./include/eolgf_updater.php');
 
 set_time_limit(0);                   // ignore php timeout
 //ignore_user_abort(true);             // keep on going even if user pulls the plug*
@@ -62,7 +45,7 @@ if($lot>0){
 
 if(count($listofnotmatch)>0){
     $listofnotmatch = array_unique($listofnotmatch);
-    echo "The following ".count($listofnotmatch)." industries has no coincidence on matching table: <br>\n";
+    echo "@jaejun<br>\nThe following ".count($listofnotmatch)." industries has no coincidence on matching table: <br>\n";
     foreach($listofnotmatch as $i => $ticker){
         echo $ticker." <br>\n";
     }
